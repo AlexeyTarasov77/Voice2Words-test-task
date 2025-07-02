fdd74eb69147022f34140fff8ca439fb4807403d
AlexeyTarasov77 <alexxey2707@gmail.com>
2025-07-02 14:37:03 +0200

    feat: add subcription entity

---
+++ src/entities/subscription/domain.ts
@@ -0,0 +1,17 @@
+export type Subscription = {
+  name: string;
+  maxRecords: number
+  total: number
+}
+
+export const FreeSubscription: Subscription = {
+  name: "Free",
+  maxRecords: 2,
+  total: 0
+}
+
+export const PaidSubscription: Subscription = {
+  name: "Paid",
+  maxRecords: -1,
+  total: 19.99
+}
