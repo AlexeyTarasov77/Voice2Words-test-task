c262b3b966aa1b8f5ac10fe54754e468f8eda1c1
AlexeyTarasov77 <alexxey2707@gmail.com>
2025-07-07 17:40:58 +0200

    chore: ignore unique violation in seed script, add it to build process

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,23 +1,32 @@
 import { SubscriptionLevel } from "@/generated/prisma"
+import { PrismaClientKnownRequestError } from "@/generated/prisma/runtime/library"
 import { prisma } from "@/shared/lib/db"
 
+const execIgnoreConflict = async (promise: Promise<any>) => {
+  try {
+    return await promise
+  } catch (err) {
+    if (!(err instanceof PrismaClientKnownRequestError && err.code == "P2002")) throw err
+  }
+}
+
 async function main() {
-  await prisma.subscription.create({
+  await execIgnoreConflict(prisma.subscription.create({
     data: {
       level: SubscriptionLevel.FREE,
       maxRecords: 2,
       features: ["Maximum 2 transcriptions", "Limited transcription time", "Standard accuracy"],
       price: 0.0
     }
-  })
-  await prisma.subscription.create({
+  }))
+  await execIgnoreConflict(prisma.subscription.create({
     data: {
       level: SubscriptionLevel.PREMIUM,
       maxRecords: -1,
       features: ["Unlimited transcriptions", "Unlimited transcription time", "High accuracy", "Priority support"],
       price: 19.99
     }
-  })
+  }))
   console.log("Created basic subscriptions")
 }
 main()
